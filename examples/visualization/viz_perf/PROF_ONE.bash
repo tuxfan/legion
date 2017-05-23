c04093b6ba51d7f45675fa1391e88f7f6078aa99
Alan Heirich <aheirich@sapling.stanford.edu>
2017-05-23 10:52:44 -0700

    dont wait during performance measurement

--- examples/visualization/viz_perf/PROF_ONE.bash
+++ examples/visualization/viz_perf/PROF_ONE.bash
@@ -4,7 +4,7 @@ cat $0
 echo =====
 touch $LG_RT_DIR/../examples/visualization/prototype1.cc
 touch $LG_RT_DIR/legion/RenderSpace.cc
-(cd $LG_RT_DIR/../examples/visualization; echo "$1 TREE_REDUCTION=1 NULL_COMPOSITE_TASKS=0 TIME_PER_FRAME=1 TIME_OVERALL=1 DEBUG=0 make" | bash)
+(cd $LG_RT_DIR/../examples/visualization; echo "$1 TREE_REDUCTION=1 NULL_COMPOSITE_TASKS=0 TIME_PER_FRAME=0 TIME_OVERALL=0 DEBUG=0 make" | bash)
 echo === RUNNING WITH "$1" ===
 rm -rf profileData
 mkdir profileData
