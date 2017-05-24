c2aaf62b678ddeb6ddb214e94a9472c22354f550
Alan Heirich <aheirich@stanford.edu>
2017-05-24 12:45:53 -0700

    different versions of usec_timer for apple versus linux

--- runtime/legion/usec_timer.h
+++ runtime/legion/usec_timer.h
@@ -25,13 +25,62 @@ using namespace std;
 
 class UsecTimer {
 public:
-    UsecTimer(string description){ mDescription = description; }
+    typedef struct timespec Time;
+    static const clockid_t CLOCK = CLOCK_MONOTONIC;
+    
+    UsecTimer(string description){
+        mDescription = description;
+        mCumulativeElapsedSeconds = 0.0;
+        mNumSamples = 0;
+        mStarted = false;
+    }
     ~UsecTimer(){}
-    void start(){}
-    void stop(){}
-    string to_string(){ return mDescription; }
+    void start(){
+        if(clock_gettime(CLOCK, &mStart)) {
+            cerr << "error from clock_gettime" << endl;
+            return;
+        }
+        mStarted = true;
+    }
+    static double timespecToSeconds(timespec *t) {
+        const double nsecToS = 1.0 / 1000000000.0;
+        return (double)t->tv_sec + (double)t->tv_nsec * nsecToS;
+    }
+    void stop(){
+        if(mStarted) {
+            Time end;
+            if(clock_gettime(CLOCK, &end)) {
+                cerr << "error from clock_gettime" << endl;
+                return;
+            }
+            double elapsedSeconds = timespecToSeconds(&end) - timespecToSeconds(&mStart);
+            mCumulativeElapsedSeconds += elapsedSeconds;
+            mNumSamples++;
+            mStarted = false;
+        }
+    }
+    string to_string(){
+        double meanSampleElapsedSeconds = 0;
+        if(mNumSamples > 0) {
+            meanSampleElapsedSeconds = mCumulativeElapsedSeconds / mNumSamples;
+        }
+        double sToUs = 1000000.0;
+        std::ostringstream output;
+        output << mDescription
+        << " " << (mCumulativeElapsedSeconds) << " sec"
+        << " " << (mCumulativeElapsedSeconds * sToUs)
+        << " usec = " << (meanSampleElapsedSeconds * sToUs)
+        << " usec * " << (mNumSamples)
+        << (mNumSamples == 1 ? " sample" : " samples");
+        return output.str();
+    }
+    
 private:
+    bool mStarted;
+    Time mStart;
     string mDescription;
+    double mCumulativeElapsedSeconds;
+    int mNumSamples;
 };
 
 
