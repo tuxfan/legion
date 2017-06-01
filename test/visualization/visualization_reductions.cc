5fb1568b6b4bdbe4b55cb53d0c4c2e0addf1685d
Alan Heirich <aheirich@stanford.edu>
2017-05-31 18:57:20 -0700

    incremental commit

--- test/visualization/visualization_reductions.cc
+++ test/visualization/visualization_reductions.cc
@@ -45,20 +45,33 @@ static void paintRegion(ImageSize imageSize,
                         int taskID) {
     
     PixelField zValue = taskID % imageSize.depth;
+    char fileName[256];
+    sprintf(fileName, "/tmp/paint.%d", taskID);
+    FILE *outputFile = fopen(fileName, "wb");
     
     for(int row = 0; row < imageSize.height; ++row) {
         for(int column = 0; column < imageSize.width; ++column) {
-            *r = taskID;
-            *g = taskID;
+            *r = row;
+            *g = column;
             *b = taskID;
             *a = taskID;
             *z = zValue;
             *userdata = taskID;
+            
+            fwrite(r, sizeof(PixelField), 1, outputFile);
+            fwrite(g, sizeof(PixelField), 1, outputFile);
+            fwrite(b, sizeof(PixelField), 1, outputFile);
+            fwrite(a, sizeof(PixelField), 1, outputFile);
+            fwrite(z, sizeof(PixelField), 1, outputFile);
+            fwrite(userdata, sizeof(PixelField), 1, outputFile);
+            
             r += stride[0]; g += stride[0]; b += stride[0]; a += stride[0]; z += stride[0]; userdata += stride[0];
             zValue = (zValue + 1);
             zValue = (zValue >= imageSize.depth) ? 0 : zValue;
         }
     }
+    
+    fclose(outputFile);
 }
 
 
@@ -76,35 +89,42 @@ void generate_image_data_task(const Task *task,
     ByteOffset stride[IMAGE_REDUCTION_DIMENSIONS];
     int layer = task->get_unique_id() % imageSize.depth;
     ImageReduction::create_image_field_pointers(imageSize, image, layer, r, g, b, a, z, userdata, stride);
-    paintRegion(imageSize, r, g, b, a, z, userdata, stride, GENERATE_IMAGE_DATA_TASK_ID);
+    int taskID = task->get_unique_id() % imageSize.depth;
+    paintRegion(imageSize, r, g, b, a, z, userdata, stride, taskID);
     render.stop();
     cout << render.to_string() << endl;
 }
 
 
-void treeTestOneOperator(ImageSize imageSize, Context context, Runtime *runtime, GLenum depthFunc, GLenum blendFuncSource, GLenum blendFuncDestination) {
+
+void verifyTestResult(int permutation[], ImageSize imageSize, GLenum depthFunc, GLenum sourceBlendFunc, GLenum destinationBlendFunc) {
+    
+}
+
+
+
+void paintImages(ImageSize imageSize, Context context, Runtime *runtime, ImageReduction &imageReduction) {
+    FutureMap generateFutures = imageReduction.launch_task_by_depth(GENERATE_IMAGE_DATA_TASK_ID);
+    generateFutures.wait_all_results();
+}
+
+
+void treeTest(ImageSize imageSize, Context context, Runtime *runtime, GLenum depthFunc, GLenum sourceBlendFunc, GLenum destinationBlendFunc) {
     ImageReduction imageReduction(imageSize, context, runtime);
+    paintImages(imageSize, context, runtime, imageReduction);
     if(depthFunc != 0) {
         imageReduction.set_depth_func(depthFunc);
     } else if(blendFuncSource != 0 && blendFuncDestination != 0) {
         imageReduction.set_blend_func(blendFuncSource, blendFuncDestination);
     }
-    FutureMap generateFutures = imageReduction.launch_task_by_depth(GENERATE_IMAGE_DATA_TASK_ID);
-    generateFutures.wait_all_results();
-    
+    int permutation[imageSize.depth];
+    generateRandomPermutation(permutation, imageSize.depth);
+    imageReduction.reduce_associative_noncommutative(permutation);
+    verifyTestResult(permutation, imageSize, depthFunc, sourceBlendFunc, destinationBlendFunc);
 }
 
 
-void treeTest(ImageSize imageSize, Context context, Runtime *runtime) {
-    for(int i = 0; i < numDepthFuncs; ++i) {
-        GLenum depthFunc = depthFuncs[i];
-        treeTestOneOperator(imageSize, context, runtime, depthFunc, 0, 0);
-    }
-
-}
-
-
-void pipelineTest(ImageSize imageSize, Context context, Runtime *runtime) {
+void pipelineTest(ImageSize imageSize, Context context, Runtime *runtime, GLenum depthFunc, GLenum sourceBlendFunc, GLenum destinationBlendFunc) {
     
 }
 
@@ -114,14 +134,27 @@ void top_level_task(const Task *task,
                     Context ctx, HighLevelRuntime *runtime) {
     
     {
-        // test with multiple fragments per scanline
+        // test with multiple fragments per scanline and all reduction operators
         int width = 64;
         int rows = 48;
         int fragmentsPerLayer = rows * 4;
         assert(fragmentsPerLayer > rows && width % (fragmentsPerLayer / rows) == 0);
         ImageSize imageSize = { width, rows, 4, fragmentsPerLayer };
-        treeTest(imageSize, ctx, runtime);
-        pipelineTest(imageSize, ctx, runtime);
+        
+        for(int i = 0; i < numDepthFuncs; ++i) {
+            GLenum depthFunc = depthFuncs[i];
+            treeTest(imageSize, ctx, runtime, depthFunc, 0, 0);
+            pipelineTest(imageSize, ctx, runtime, depthFunc, 0, 0);
+        }
+        
+        for(int i = 0; i < numBlendFuncs; ++i) {
+            GLenum sourceFunc = blendFuncs[i];
+            for(int j = 0; j < numBlendFuncs; ++j) {
+                GLenum destinationFunc = blendFuncs[j];
+                treeTest(imageSize, ctx, runtime, 0, sourceFunc, destinationFunc);
+                pipelineTest(imageSize, ctx, runtime, 0, sourceFunc, destinationFunc);
+            }
+        }
     }
     
     {
