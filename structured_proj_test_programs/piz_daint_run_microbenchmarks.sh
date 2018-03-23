dc5be6bd68d137fc9210b9d6e59ec789dedc4d84
Todd Warszawski <twarsz1414@gmail.com>
2018-03-23 07:39:04 +0100

    Updating script to set window and to print extra dates.

--- structured_proj_test_programs/piz_daint_run_microbenchmarks.sh
+++ structured_proj_test_programs/piz_daint_run_microbenchmarks.sh
@@ -18,12 +18,16 @@ for ((trial=1; trial<=$TRIALS; trial++ ))
 do
   for nodes in 1 2 4 8 16 32 64
   do
+    date
     dim=`expr 8 \* $nodes`
+    window=20000
+    #window=`expr $dim \* $dim`
+    #window=$(($window + dim))
     for sleep_us in 0 1000 10000
     do
       affine_command="srun -n $nodes -N $nodes --tasks-per-node 1 --cpu_bind none ./x_parallel_only/runtime_stress_test -i $ITERATIONS -nx $dim -bx $dim -ny $dim -by $dim -sms $sleep_us -sm -ssl 8 -a 225 -ll:cpu $cpus_per_node -ll:util $util_per_node -ll:io $io_per_node"
       index_command="srun -n $nodes -N $nodes --tasks-per-node 1 --cpu_bind none ./current_optimum_angle/current_optimum_angle -i $ITERATIONS -nx $dim -bx $dim -ny $dim -by $dim -sms $sleep_us -ll:cpu $cpus_per_node -ll:util $util_per_node -ll:io $io_per_node"
-      no_proj_command="srun -n $nodes -N $nodes --tasks-per-node 1 --cpu_bind none ./zero_projections/zero_projections -i $ITERATIONS -nx $dim -bx $dim -ny $dim -by $dim -sms $sleep_us -a 225 -ll:cpu $cpus_per_node -ll:util $util_per_node -ll:io $io_per_node"
+      no_proj_command="srun -n $nodes -N $nodes --tasks-per-node 1 --cpu_bind none ./zero_projections/zero_projections -i $ITERATIONS -nx $dim -bx $dim -ny $dim -by $dim -sms $sleep_us -a 225 -ll:cpu $cpus_per_node -ll:util $util_per_node -ll:io $io_per_node -lg:window $window"
 
       echo $affine_command
       $affine_command
@@ -39,7 +43,7 @@ do
     do
       affine_command="srun -n $nodes -N $nodes --tasks-per-node 1 --cpu_bind none ./x_parallel_only/runtime_stress_test -i $ITERATIONS -nx 1 -bx 1 -ny $dim -by $dim -sms $sleep_us -a 180 -ll:cpu $cpus_per_node -ll:util $util_per_node -ll:io $io_per_node"
       index_command="srun -n $nodes -N $nodes --tasks-per-node 1 --cpu_bind none ./current_optimum_axis_aligned/current_optimum_axis_aligned -i $ITERATIONS -nx 1 -bx 1 -ny $dim -by $dim -sms $sleep_us -ll:cpu $cpus_per_node -ll:util $util_per_node -ll:io $io_per_node"
-      no_proj_command="srun -n $nodes -N $nodes --tasks-per-node 1 --cpu_bind none ./zero_projections/zero_projections -i $ITERATIONS -nx 1 -bx 1 -ny $dim -by $dim -sms $sleep_us -a 180 -ll:cpu $cpus_per_node -ll:util $util_per_node -ll:io $io_per_node"
+      no_proj_command="srun -n $nodes -N $nodes --tasks-per-node 1 --cpu_bind none ./zero_projections/zero_projections -i $ITERATIONS -nx 1 -bx 1 -ny $dim -by $dim -sms $sleep_us -a 180 -ll:cpu $cpus_per_node -ll:util $util_per_node -ll:io $io_per_node -lg:window $window"
 
       echo $affine_command
       $affine_command
